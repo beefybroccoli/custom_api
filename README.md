e04faec569ed009379e37753979b08a519452b76
beefybroccoli <tedkimemail@yahoo.com>
2021-12-02 15:11:14 -0800

    milestone 1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # custom_api
+
+### milestone 1
+    _install dependencies : node, nodemon, eslint, dotenv, cors
+    _create and modify index.js and server.js
+    _start the API with helloworld message
---
+++ api/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.get("/", (req, res)=>{
+    res.status(200).json({message:"hello world"});
+})
+
+module.exports = server;
