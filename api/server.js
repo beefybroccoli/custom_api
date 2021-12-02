0457af9f64be9d209cd7025103cc1fdfdeca71fe
beefybroccoli <tedkimemail@yahoo.com>
2021-12-02 15:27:15 -0800

    milestone 2

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const server = express();
 const routerUsers = require("./users/router-users");
+
 server.use(express.json());
 server.use(express.Router());
-
 server.use("/api/users", routerUsers);
 
 server.get("/", (req, res)=>{
