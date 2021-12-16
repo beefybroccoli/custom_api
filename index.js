4ce2da6386a64bf67acb3462224cbdc1e9895fe9
beefybroccoli <tedkimemail@yahoo.com>
2021-12-15 18:17:41 -0800

    ...

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, ()=>{
     console.log(`listening on PORT ${PORT}`);
+    console.log("process.env.NODE_ENV = ", process.env.NODE_ENV);
+    console.log("process.env.DEV_DATABASE_URL = ", process.env.DEV_DATABASE_URL);
 })  
